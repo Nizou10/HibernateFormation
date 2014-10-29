0d1b419912abb9577c7a8ec7994fd6aa5d2d261f
ATEC-Nizar <ATEC-Nizar@ATEC-Nizar-PC>
2014-10-29 23:33:22 +0100

    first Nizar modifications

---
+++ HibernateFormation/src/com/formation/hibernate/Student_Info.java
@@ -0,0 +1,50 @@
+package com.formation.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="STUDENT_INFORMATION")
+public class Student_Info {
+
+	@Id 
+	/****hibernate autogenerate the id****/
+	@GeneratedValue(strategy= GenerationType.IDENTITY) /*** IDENTITY for my sql database***/
+	private int    roll_no;
+	
+	/** Some other hibernate annotation***/
+	@Column(name="name", nullable=false)
+	private String name;
+
+	public Student_Info() {
+		
+	}
+	
+	public Student_Info(String name, int roll_no) {
+		
+		this.name    = name;
+		this.roll_no = roll_no;
+	}
+
+	public int getRoll_no() {
+		return roll_no;
+	}
+
+	public void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
